05752d6e326bd609a00fdc1d36c93b1309d654b7
Maryust <masyanya1101@yandex.ru>
2021-12-14 21:25:01 +0300

    radio update

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,11 +1,20 @@
 package ru.netology;
 
 public class Radio {
+    private int amountOfRadioStation = 10;
     private int numberRadioStation;
     private int volume;
 
+    public Radio() {
+    }
+
+    public Radio(int amountOfRadioStation) {
+        this.amountOfRadioStation = amountOfRadioStation;
+
+    }
+
     public void setNumberRadioStation(int numberRadioStation) {
-        if (numberRadioStation > 9) {
+        if (numberRadioStation > amountOfRadioStation - 1) {
             return;
         }
         if (numberRadioStation < 0) {
@@ -18,8 +27,12 @@ public class Radio {
         return numberRadioStation;
     }
 
+    public int getAmountOfRadioStation() {
+        return amountOfRadioStation;
+    }
+
     public int increaseStation() {
-        if (numberRadioStation < 9) {
+        if (numberRadioStation < amountOfRadioStation - 1) {
             numberRadioStation = numberRadioStation + 1;
         } else numberRadioStation = 0;
         return numberRadioStation;
@@ -28,7 +41,7 @@ public class Radio {
     public int reduceStation() {
         if (numberRadioStation > 0) {
             numberRadioStation = numberRadioStation - 1;
-        } else numberRadioStation = 9;
+        } else numberRadioStation = amountOfRadioStation - 1;
         return numberRadioStation;
     }
 
@@ -37,9 +50,9 @@ public class Radio {
     }
 
     public int increaseVolume() {
-        if (volume < 10) {
+        if (volume < 100) {
             volume = volume + 1;
-        } else volume = 10;
+        } else volume = 100;
         return volume;
     }
 
